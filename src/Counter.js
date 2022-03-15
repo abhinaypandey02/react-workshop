6db0d5b911159f304ab68c09ed90ac002cb5b4ad
Abhinay Pandey <abhinaypandey02@gmail.com>
2022-03-15 19:02:44 +0530

    Update Counter.js

--- src/Counter.js
+++ src/Counter.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect, useRef } from 'react'
 
 export default function CounterHooks({ initialCount }) {
   const [count, setCount] = useState(initialCount)
-  const [even, setEven] = useState(count%2===0)
+  const [even, setEven] = useState(count%2===0);
+  const [logs,setLogs] = useState([]);
   const resetVal = useRef(null)
   useEffect(() => {
     setEven(count%2===0)
@@ -12,17 +13,26 @@ export default function CounterHooks({ initialCount }) {
       Reset Counter Value
       <form onSubmit={(e) => {
         e.preventDefault()
-        if(resetVal.current.value!=='')
-        setCount(parseInt(resetVal.current.value))
+        if(resetVal.current.value!=='') {
+            const val=parseInt(resetVal.current.value);
+            setCount(val)
+            setLogs(old=>[...old,val])
+        }
       }}>
-        <input type={"number"} ref={resetVal}></input>
+        <input type={"number"} ref={resetVal}/>
         <button type='submit'>Reset Counter Value</button>
-      </form>      
+      </form>
       <div>
         <button onClick={() => setCount(prevCount => prevCount - 1)}>-</button>
         <span>{count}</span>
         <button onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
         {even && <div>The current count is even</div>}
+          <h4>Logs:</h4>
+          <div>
+              {logs.map((log,index)=><div key={index}>
+                  Reset to {log}
+              </div>)}
+          </div>
       </div>
     </>
   )
